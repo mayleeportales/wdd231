1c0a0e8d77af53d29129f9b59d064c681e96f02a
mayleeportales <maylee.portales@gmail.com>
2026-09-14 18:09:15 -0300

    W02 Chamber DIrectory Page

---
+++ chamber/scripts/members.js
@@ -0,0 +1,94 @@
+const url = "data/members.json";
+
+const cards = document.querySelector('#cards');
+
+const gridButton = document.querySelector('.grid-btn');
+const listButton = document.querySelector('.list-btn');
+
+gridButton.addEventListener('click', () => {
+    cards.classList.add('grid');
+    cards.classList.remove('list');
+}) 
+
+listButton.addEventListener('click', () => {
+    cards.classList.add('list');
+    cards.classList.remove('grid');
+})
+
+async function getMembersData() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data  = await response.json();
+            displayMembers(data.companies);
+        } else {
+            throw Error("Error fetching data.");
+        }
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }    
+}
+
+const displayMembers = (members) => {
+    members.forEach(member => {
+        const card = document.createElement('section');
+        card.classList.add('card');
+
+        const companyName = document.createElement('h2');
+        companyName.textContent = `${member.name}`;
+
+        const companyTagline = document.createElement('p');
+        companyTagline.textContent = `${member.tagline}`;
+        
+        const labelAddress = document.createElement('strong');
+        labelAddress.textContent = 'Address: ';
+
+        const companyAddress = document.createElement('p');
+        companyAddress.appendChild(labelAddress);
+        companyAddress.append(member.address);
+        
+        const labelNumber = document.createElement('strong');
+        labelNumber.textContent = 'Phone: ';
+
+        const companyPhoneNumber = document.createElement('p');
+        companyPhoneNumber.appendChild(labelNumber);
+        companyPhoneNumber.append(member.phoneNumber);
+
+        const companyUrl = document.createElement('a');
+        companyUrl.setAttribute('href', member.url);
+        companyUrl.setAttribute('target', '_blank')
+        companyUrl.textContent = new URL(member.url).hostname.replace("www.", "");
+
+        const companyImage = document.createElement('img');
+        companyImage.setAttribute('src', `images/${member.image}`);
+        companyImage.setAttribute('alt', "");
+        companyImage.setAttribute('loading', 'lazy');
+        companyImage.setAttribute('width', '600');
+        companyImage.setAttribute('height', '400');
+
+        const cardHeader = document.createElement('div');
+        cardHeader.appendChild(companyName);
+        cardHeader.appendChild(companyTagline);
+        cardHeader.classList.add('card-header');
+
+        const cardBody = document.createElement('div');
+        cardBody.appendChild(companyImage);
+
+        const cardInfo = document.createElement('div');
+        cardInfo.appendChild(companyAddress);
+        cardInfo.appendChild(companyPhoneNumber);
+        cardInfo.appendChild(companyUrl); 
+        cardInfo.classList.add('card-info');
+
+        cardBody.classList.add('card-body');
+
+        cardBody.appendChild(cardInfo);
+        
+        
+        card.appendChild(cardHeader);
+        card.appendChild(cardBody);
+
+        cards.appendChild(card);
+    });    
+}
+getMembersData();
